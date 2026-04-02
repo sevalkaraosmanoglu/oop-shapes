59082d622de5829030f870df84e39d0e7bcf07b3
Seval Karaosmanoğlu <sevallacin@gmail.com>
2026-04-02 15:35:34 +0300

    Corrected say_name outputs for Shape, Rectangle, Circle to pass tests

--- shape.py
+++ shape.py
@@ -1,2 +1,30 @@
 # pylint: disable=too-few-public-methods missing-module-docstring
-pass  # YOUR CODE HERE
+class Shape:
+    def __init__(self, color, name):
+        self.color = color
+        self.name = name
+    def say_name(self):
+        return f"My name is {self.name}."
+
+class Rectangle(Shape):
+    def __init__(self, color, name, width, height):
+        super().__init__(color, name)
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.height
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+    def say_name(self):
+        return f"My name is {self.name} and I am a rectangle."
+
+class Circle(Shape):
+    def __init__(self, color, name, radius):
+        super().__init__(color, name)
+        self.radius = radius
+    def area(self):
+        return 3.14 * self.radius ** 2
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+    def say_name(self):
+        return f"My name is {self.name} and I am a circle."
